41745dd88a59ea22841ff6d3ed87afadd18cbc3b
hyun <coderhyun476@gmail.com>
2020-04-06 23:27:37 +0900

    네비게이션 연동

---
+++ app/src/screens/HomeScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HOME</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeScreen
---
+++ app/src/screens/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import HomeScreen from './HomeScreen';
+
+
+const Stack = createStackNavigator();
+
+const AppContainer = () =>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+
+export default AppContainer
