d10d78142877703001c45b08caf22a9efdcf68ba
hyun <coderhyun476@gmail.com>
2020-04-09 23:36:29 +0900

    ads 연동

--- app/App.tsx
+++ app/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppContainer from './src/screens';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import configureStore from './src/redux';
-
+import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
 
 
 const { store, persistor } = configureStore();
 
 const App = () => {
+
+  const adsInit = async () => {
+    try {
+      await admob()
+        .setRequestConfiguration({
+          maxAdContentRating: MaxAdContentRating.PG,
+          tagForChildDirectedTreatment: true,
+          tagForUnderAgeOfConsent: true,
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    adsInit()
+  }, [])
+
   return (
     <>
       <Provider store={store}>
--- app/src/screens/HomeScreen/index.tsx
+++ app/src/screens/HomeScreen/index.tsx
@@ -3,45 +3,86 @@ import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
 import BackgroundAudioRecord from '../../modules/BackgroundAudioRecord'
 import { BaseButton } from 'react-native-gesture-handler'
 import moment from 'moment'
+import { InterstitialAd, TestIds, AdEventType, RewardedAd, RewardedAdEventType, BannerAdSize, BannerAd } from '@react-native-firebase/admob'
+
+const interstitial = InterstitialAd.createForAdRequest(TestIds.INTERSTITIAL, {
+    requestNonPersonalizedAdsOnly: true,
+});
+
+const rewarded = RewardedAd.createForAdRequest(TestIds.REWARDED, {
+    requestNonPersonalizedAdsOnly: true
+});
 
 const HomeScreen = () => {
+    const [interstitialLoad, setInterstitialLoad] = useState(false);
+    const [rewardedLoad, setRewardedLoad] = useState(false);
+
     useEffect(() => {
         console.log("load")
         BackgroundAudioRecord.startService()
     }, [])
 
+    useEffect(() => {
+        const eventListener = interstitial.onAdEvent(type => {
+            if (type === AdEventType.LOADED) {
+                setInterstitialLoad(true);
+            }
+            if (type === AdEventType.OPENED) {
+                BackgroundAudioRecord.saveRecording(900, "Recording-" + Date.now(), (state: string) => console.log(state))
+            }
+        });
+
+        // Start loading the interstitial straight away
+        interstitial.load();
+
+        // Unsubscribe from events on unmount
+        return () => {
+            eventListener();
+        };
+    }, []);
+
+    useEffect(() => {
+        const eventListener = rewarded.onAdEvent((type, error, reward) => {
+            if (type === RewardedAdEventType.LOADED) {
+                setRewardedLoad(true);
+            }
+
+            if (type === RewardedAdEventType.EARNED_REWARD) {
+                console.log('User earned reward of ', reward);
+                BackgroundAudioRecord.saveRecording(1800, "Recording-" + Date.now(), (state: string) => console.log(state))
+            }
+        });
+
+        rewarded.load();
+
+        return () => {
+            eventListener();
+        };
+    }, []);
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingVertical: 200, }} >
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingVertical: 200 }} >
+            <BannerAd
+                unitId={TestIds.BANNER}
+                size={BannerAdSize.LARGE_BANNER}
+                requestOptions={{
+                    requestNonPersonalizedAdsOnly: true,
+                }}
+            />
             <BaseButton
                 onPress={() => {
-                    try {
-                        BackgroundAudioRecord.saveRecording(300, "Recording-" + moment().format("YYYY-MM-DD-h:mm:ss"), (state: string) => console.log(state))
-                    } catch (error) {
-
-                    }
+                    BackgroundAudioRecord.saveRecording(300, "Recording-" + moment().format("YYYY-MM-DD-h:mm:ss"), (state: string) => console.log(state))
                 }}
             >
                 <Text>5분 저장 Save Recording</Text>
             </BaseButton>
             <TouchableWithoutFeedback
-                onPress={() => {
-                    try {
-                        BackgroundAudioRecord.saveRecording(900, "Recording-" + Date.now(), (state: string) => console.log(state))
-                    } catch (error) {
-
-                    }
-                }}
+                onPress={() => { interstitial.show() }}
             >
                 <Text>15분 저장 Save Recording</Text>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
-                onPress={() => {
-                    try {
-                        BackgroundAudioRecord.saveRecording(1800, "Recording-" + Date.now(), (state: string) => console.log(state))
-                    } catch (error) {
-
-                    }
-                }}
+                onPress={() => { rewarded.show(); }}
             >
                 <Text>30분 저장 Save Recording</Text>
             </TouchableWithoutFeedback>
